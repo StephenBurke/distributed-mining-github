3c851b9a9f005583f92f4880f217e4d3675c3818
StephenBurke <Burke.stephenpaul@gmail.com>
2021-03-11 18:37:25 -0600

    Added gh_graph_utils.py with methods

---
+++ gh_graph_utils.py
@@ -0,0 +1,14 @@
+def augment_owns(graph, user):
+    graph.add_node(user.login, node_type = 'user')
+    repos = user.get_repos()
+    for repo in repos:
+        graph.add_node(repo.name, node_type = 'repo')
+        graph.add_edge(user.login, repo.name, edge_type = 'owns')
+    return repos
+
+def augment_owns(graph, repo):
+    gazers = repo.get_stargazers()
+    for stargazer in gazers:
+        graph.add_node(stargazer.login, node_type = 'user')
+        graph.add_edge(stargazer.login, repo.name, edge_type = 'gazes')
+    return repos
